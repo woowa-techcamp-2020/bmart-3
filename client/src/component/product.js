ae99e24bb35fd7747e39ee7ccd0f2291e9b49d25
haerang <gofkdvjvl@naver.com>
2020-08-13 17:39:50 +0900

    feat: 제품 페이지 마크업 작성
    - 제품 컴포넌트 ui 작성 및 css 적용

--- client/src/component/product.js
+++ client/src/component/product.js
@@ -4,44 +4,80 @@ import "./Product.scss";
 const Product = () => {
   return (
     <>
-      <h1>분식 야식</h1>
-      <button>더보기></button>
+      <div className="product-container-header">
+        <h1>분식 야식</h1>
+        <button>더보기></button>
+      </div>
       <div className="product-container">
         <div className="product-item">
           <img src="https://source.unsplash.com/random"></img>
+          <div>
+            <p>한입 쏙 비엔나</p>
+            <p>1190원</p>
+          </div>
         </div>
         <div className="product-item">
           <img src="https://source.unsplash.com/random"></img>
+          <div>
+            <p>한입 쏙 비엔나</p>
+            <p>1190원</p>
+          </div>
         </div>
         <div className="product-item">
           <img src="https://source.unsplash.com/random"></img>
+          <div>
+            <p>한입 쏙 비엔나</p>
+            <p>1190원</p>
+          </div>
         </div>
         <div className="product-item">
           <img src="https://source.unsplash.com/random"></img>
+          <div>
+            <p>한입 쏙 비엔나</p>
+            <p>1190원</p>
+          </div>
         </div>
         <div className="product-item">
           <img src="https://source.unsplash.com/random"></img>
+          <div>
+            <p>한입 쏙 비엔나</p>
+            <p>1190원</p>
+          </div>
         </div>
         <div className="product-item">
           <img src="https://source.unsplash.com/random"></img>
+          <div>
+            <p>한입 쏙 비엔나</p>
+            <p>1190원</p>
+          </div>
         </div>
         <div className="product-item">
           <img src="https://source.unsplash.com/random"></img>
+          <div>
+            <p>한입 쏙 비엔나</p>
+            <p>1190원</p>
+          </div>
         </div>
         <div className="product-item">
           <img src="https://source.unsplash.com/random"></img>
+          <div>
+            <p>한입 쏙 비엔나</p>
+            <p>1190원</p>
+          </div>
         </div>
         <div className="product-item">
           <img src="https://source.unsplash.com/random"></img>
+          <div>
+            <p>한입 쏙 비엔나</p>
+            <p>1190원</p>
+          </div>
         </div>
         <div className="product-item">
           <img src="https://source.unsplash.com/random"></img>
-        </div>
-        <div className="product-item">
-          <img src="https://source.unsplash.com/random"></img>
-        </div>
-        <div className="product-item">
-          <img src="https://source.unsplash.com/random"></img>
+          <div>
+            <p>한입 쏙 비엔나</p>
+            <p>1190원</p>
+          </div>
         </div>
       </div>
     </>
