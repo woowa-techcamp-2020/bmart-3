fd138935e9de851d37c5ce4fc95f91b4cdc83695
LimHaksu <limhaksu@naver.com>
2020-08-24 19:00:12 +0900

    fix: ssh2 사용 안함
    도커를 사용해서 로컬작업을 하므로 ssh2를 통해서 프라이빗 DB에 접근할 필요가 없음

--- server/app.js
+++ server/app.js
@@ -52,5 +52,3 @@ const port = Number(process.env.PORT || 3000);
 app.listen(port, () => {
   console.log('Express server started on port: ' + port);
 });
-
-export default app;
--- server/db/share/db.config.js
+++ server/db/share/db.config.js
@@ -1,16 +1,6 @@
-import path from 'path';
 import dotenv from 'dotenv';
-import fs from 'fs';
 
 dotenv.config();
-
-const sshConf = {
-  host: process.env.SSH_HOST,
-  port: process.env.SSH_PORT,
-  user: process.env.SSH_USER,
-  readyTimeout: 99999,
-  privateKey: fs.readFileSync(path.join(__dirname, 'bmart-3.pem')), //리눅스에서 ssh접속시 사용할 키!!
-};
 
 const dbConf = {
   host: process.env.DB_HOST,
@@ -20,4 +10,4 @@ const dbConf = {
   database: process.env.DB_DATABASE,
 };
 
-export { sshConf, dbConf };
+export { dbConf };
